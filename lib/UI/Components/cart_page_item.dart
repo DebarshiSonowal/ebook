50eddebfcd16947f0a6fb9660e276809bd6a6733
Debarshi Sonowal <debarkhisonowal@gmail.com>
2023-01-30 22:20:32 +0530

    Changes

---
+++ lib/UI/Components/cart_page_item.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CartPageItem extends StatelessWidget {
+  const CartPageItem({Key? key}) : super(key: key);
+ 
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
